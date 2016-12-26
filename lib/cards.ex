50d4017c91d87fee5a2da4df87c156866c8fa481
ariapahlavan <apahlavan1@utexas.edu>
2016-12-26 16:17:00 -0600

    Implement create_deck, shuffle, and contains? functions

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,3 +1,28 @@
 defmodule Cards do
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
+    # Solution No. 2
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+
+    # Solution No. 1
+    # cards = for value <- values do
+    #   for suit <- suits do
+    #     "#{value} of #{suit}"
+    #   end
+    # end
+    #
+    # List.flatten(cards)
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
 end
