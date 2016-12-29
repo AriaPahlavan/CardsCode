b9c5ead419b6384961eb64ebb05dc7d4cdc33117
ariapahlavan <apahlavan1@utexas.edu>
2016-12-28 22:43:41 -0600

    Add ex_docs to dependencies and add dcumentions for Cards module and its fuctions

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,12 @@
 defmodule Cards do
+  @moduledoc """
+    Provides functions for creating and handling a deck of cards
+  """
+
+
+  @doc """
+    Creates a deck of cards.
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five",
               "Six", "Seven", "Eight", "Nine", "Ten",
@@ -24,19 +32,40 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+
+  @doc """
+    Devides `deck` into a hand and the remainder of the deck.
+    the `hand_size` argument indicated how many cards should
+    be in the hand.
+
+  ## Examples
+
+      iex> deck = Cards.create_hand
+      iex> {hand, remainder} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
+
+  @doc """
+    Saves the `deck` in `filename`.
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
 
+  @doc """
+    Loads the deck from `filename`.
+  """
   def load(filename) do
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term binary
@@ -46,6 +75,19 @@ defmodule Cards do
     end
   end
 
+
+  @doc """
+    Creates a deck of card, shuffles them, and devides
+    the deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicated how many cards
+    should be in the hand.
+
+  ## Examples
+
+      iex> {hand, remainder} = Cards.create_hand(1)
+      iex> hand
+      ["Jack of Clubs"]
+  """
   def create_hand(hand_size) do
     Cards.create_deck
     |> Cards.shuffle
